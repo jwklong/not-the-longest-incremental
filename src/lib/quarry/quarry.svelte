fe62078dacaa0f2d72248d55cff30d6ae6a0dacf
jwklong <jwklong@gmx.com>
2025-03-28 22:26:52 +0000

    placeholder quarry element

---
+++ src/lib/quarry/quarry.svelte
@@ -0,0 +1,33 @@
+<script>
+    import data from "$lib/data/data";
+    import onum from "$lib/onum";
+    import { onMount } from "svelte";
+    import globalUpdater from "$lib/globalUpdater";
+
+    let {
+        id = ""
+    } = $props()
+
+    let quarry = $state(data.quarrys[id])
+
+    onMount(() => {
+        globalUpdater.addUpdate(() => {
+            
+        })
+    });
+</script>
+
+<div class="main">
+    
+</div>
+
+<style>
+    .main {
+        display: flex;
+        width: 350px;
+        height: 300px;
+        background: #444;
+        box-shadow: inset 0 0 15px #0008;
+        border-radius: 8px;
+    }
+</style>
---
+++ src/lib/tabs/quarry/mining.svelte
@@ -0,0 +1,5 @@
+<script>
+    import Quarry from "$lib/quarry/quarry.svelte";
+</script>
+
+<Quarry id="normal" />
