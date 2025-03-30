425f9df17e074af797ba8b11aea5764080db788e
jwklong <jwklong@gmx.com>
2025-03-30 14:59:08 +0000

    more oar

--- src/lib/data/layers/quarry.js
+++ src/lib/data/layers/quarry.js
@@ -21,6 +21,18 @@ let quarry = new Quarry("normal", {
             name: "Stone",
             health: onum(3),
             value: [onum(0.05), "euros"]
+        },
+        {
+            id: "coal",
+            name: "Coal",
+            health: onum(4),
+            value: [onum(0.08), "euros"]
+        },
+        {
+            id: "copper",
+            name: "Copper",
+            health: onum(6),
+            value: [onum(0.1), "euros"]
         }
     ],
     layers: [
@@ -29,7 +41,9 @@ let quarry = new Quarry("normal", {
             name: "Surface",
             ores: [
                 {id: "dirt", rarity: 1},
-                {id: "stone", rarity: 0.5}
+                {id: "stone", rarity: 0.5},
+                {id: "coal", rarity: 0.2},
+                {id: "copper", rarity: 0.2}
             ]
         }
     ],
