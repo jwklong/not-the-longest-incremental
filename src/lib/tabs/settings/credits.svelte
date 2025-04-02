929488179ab2f83719d6bb1eea91847dfb7f73b4
jwklong <jwklong@gmx.com>
2025-04-02 16:23:22 +0000

    put discordo in credits

--- src/lib/tabs/settings/credits.svelte
+++ src/lib/tabs/settings/credits.svelte
@@ -1,4 +1,6 @@
 <div>
     <i>made by <b>jwklong</b></i>
+    <span>join my new and awesome <a href="https://discord.gg/NeYug9v2hq" target="_blank">discord server</a>!</span>
+    <br />
     <span>play <a href="https://galaxy.click/play/9" target="_blank">TLI</a> and <a href="https://galaxy.click/play/73" target="_blank">TLI 2</a> (not made by me but i was one of the devs)</span>
 </div>
